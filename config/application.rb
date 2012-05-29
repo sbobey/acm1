9eb53a272d78505ce2b94603de7e496804dfafd1
Steve <steve@hdadevtmm.saps>
2012-05-29 00:35:12 -0400

    wip

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Acm1
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+	config.active_record.timestamped_migrations = false	
+	
   end
 end
