2aa92fd1601983d7eba5548095704245aa310bc1
Steve <steve@hdadevtmm.saps>
2012-07-27 03:16:48 -0400

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_to_https 
+  
   def redirect_to(options = {}, response_status = {})
      if request.xhr?
        render(:update) {|page| page.redirect_to(options)}
@@ -7,5 +9,11 @@ class ApplicationController < ActionController::Base
        super(options, response_status)
      end
   end
+  
+  def redirect_to_https
+#   redirect_to :protocol => "https://" unless (request.ssl? || local_request? || Rails.env.development?)
+    redirect_to :protocol => "https://" unless (request.ssl? || Rails.env.development?)
+  end
+   
  
 end
